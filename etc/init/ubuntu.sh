59518ecad5cbc373b0e6736579b14027fc8631bc
icebee <nagamine.k.aa@gmail.com>
2018-12-22 09:56:55 +0900

    ch sh

--- etc/init/ubuntu.sh
+++ etc/init/ubuntu.sh
@@ -9,8 +9,11 @@ apt update && apt upgrade -y
 apt install -y tmux htop tree wget curl cmake gcc g++ build-essential ca-certificates software-properties-common
 
 # zsh
-curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
+apt install zsh
 touch dotfiles/zsh/.zshrc_local
+curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
+ln -s $HOME/dotfiles/zsh/.zshenv $HOME/
+chsh $USER -s $(which zsh)
 
 # pyenv
 git clone https://github.com/yyuu/pyenv.git $HOME/.local/share/.pyenv
@@ -27,3 +30,6 @@ pip install neovim
 # ssh
 ssh-keygen -t rsa -b 4096 -C $EMALE
 chmod 600 $HOME/.ssh/id_rsa
+
+# clean up
+rm -f $HOME/.bash_history $HOME/.bash_logout $HOME/.bashrc

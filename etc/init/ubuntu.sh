35726958c4e276a155ac4cfa6c509a7c6ddcc5f9
icebee <nagamine.k.aa@gmail.com>
2018-12-22 10:23:06 +0900

    ch sh

--- etc/init/ubuntu.sh
+++ etc/init/ubuntu.sh
@@ -16,8 +16,8 @@ apt update && apt upgrade -y
 apt install -y tmux htop tree wget curl cmake gcc g++ build-essential ca-certificates software-properties-common
 
 # zsh
-apt install zsh
-touch dotfiles/zsh/.zshrc_local
+apt install zsh -y
+touch $HOME/dotfiles/zsh/.zshrc_local
 curl -sL --proto-redir -all,https https://raw.githubusercontent.com/zplug/installer/master/installer.zsh| zsh
 ln -s $HOME/dotfiles/zsh/.zshenv $HOME/
 chsh $USER -s $(which zsh)
@@ -42,3 +42,4 @@ chmod 600 $HOME/.ssh/id_rsa
 
 # clean up
 rm -f $HOME/.bash_history $HOME/.bash_logout $HOME/.bashrc
+exit

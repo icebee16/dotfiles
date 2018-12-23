8c5a287feb12169689f0645594156d12cf931d3c
icebee <nagamine.k.aa@gmail.com>
2018-12-23 15:31:16 +0900

    ch sh

--- etc/init/zsh_ubuntu.sh
+++ etc/init/zsh_ubuntu.sh
@@ -1,7 +1,3 @@
-# variance
-echo "give me e-mail"
-read EMAIL
-
 # XDG Base Directory
 # https://wiki.archlinux.org/index.php/XDG_Base_Directory
 XDG_CONFIG_HOME=$HOME/.config
@@ -10,7 +6,7 @@ XDG_DATA_HOME=$HOME/.local/share
 
 # pyenv
 git clone https://github.com/yyuu/pyenv.git $XDG_DATA_HOME/.pyenv
-apt install -y make libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev
+sudo apt install -y make libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev
 set +e
 source $ZDOTDIR/.zshrc
 set -e
@@ -20,13 +16,15 @@ pyenv global 3.6.6
 # neovim
 mkdir $XDG_CONFIG_HOME/nvim
 ln -s $HOME/dotfiles/vim/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
-apt-add-repository ppa:neovim-ppa/stable
-apt update
-apt install -y neovim
+sudo apt-add-repository ppa:neovim-ppa/stable
+sudo apt update
+sudo apt install -y neovim
 pip install neovim
 
 # ssh
-ssh-keygen -t rsa -b 4096 -C $EMAIL
+echo "give me e-mail"
+read EMAIL
+ssh-keygen -f $HOME/.ssh/id_rsa -t rsa -b 4096 -C $EMAIL
 chmod 600 $HOME/.ssh/id_rsa
 
 # clean up

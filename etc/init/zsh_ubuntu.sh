ab9576eda7f692e22c222f007e81fe3e042e7c36
icebee <nagamine.k.aa@gmail.com>
2019-05-04 18:26:53 +0900

    make IntelMKL and numpy, scipy build section

--- etc/init/zsh_ubuntu.sh
+++ etc/init/zsh_ubuntu.sh
@@ -1,21 +1,36 @@
-# XDG Base Directory
+# ================== #
+# XDG Base Directory #
+# ================== #
 # https://wiki.archlinux.org/index.php/XDG_Base_Directory
 XDG_CONFIG_HOME=$HOME/.config
 XDG_CACHE_HOME=$HOME/.cache
 XDG_DATA_HOME=$HOME/.local/share
 
-# pyenv
+# ======== #
+# IntelMKL #
+# ======== #
+# https://software.intel.com/en-us/articles/installing-intel-free-libs-and-python-apt-repo
+wget https://apt.repos.intel.com/intel-gpg-keys/GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
+apt-key add GPG-PUB-KEY-INTEL-SW-PRODUCTS-2019.PUB
+wget https://apt.repos.intel.com/setup/intelproducts.list -O /etc/apt/sources.list.d/intelproducts.list
+sudo apt update
+sudo apt install intel-mkl-2019.3-062
+
+# ====== #
+# python #
+# ====== #
+# >> pyenv
 git clone https://github.com/yyuu/pyenv.git $XDG_DATA_HOME/.pyenv
-sudo apt install -y make libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev
+sudo apt-get install -y make libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev
 set +e
 source $ZDOTDIR/.zshrc
 set -e
 pyenv install 3.6.8
 pyenv global 3.6.8
-pip install pip==18.0
+pip install pip==19.3
 pip install pipenv
 
-# neovim
+# >> neovim
 mkdir $XDG_CONFIG_HOME/nvim
 ln -s $HOME/dotfiles/vim/.vimrc $XDG_CONFIG_HOME/nvim/init.vim
 sudo apt-add-repository ppa:neovim-ppa/stable
@@ -24,20 +39,34 @@ sudo apt install -y neovim
 pip install neovim
 pip install flake8
 
-# jupyter
+# >> jupyter
 pip install jupyter
 pip install jupyter_contrib_nbextensions
 pip install jupyterthemes
 jt -t chesterish -T -f roboto -fs 9 -tf merriserif -tfs 11 -nf ptsans -nfs 11 -dfs 8 -ofs 8
 
-# kaggle
+# >> kaggle
 pip install kaggle
 
-# ssh
+# >> numpy, scipy (IntelMKL build)
+touch $HOME/.numpy-site.cfg
+echo '[mkl]' >> $HOME/.numpy-site.cfg
+echo 'library_dirs = /opt/intel/mkl/lib/intel64' >> $HOME/.numpy-site.cfg
+echo 'include_dirs = /opt/intel/mkl/include' >> $HOME/.numpy-site.cfg
+echo 'mkl_libs = mkl_rt' >> $HOME/.numpy-site.cfg
+echo 'lapack_libs =' >> $HOME/.numpy-site.cfg
+pip install --no-binary :all: numpy
+pip install --no-binary :all: scipy
+
+# === #
+# ssh #
+# === #
 echo "give me e-mail"
 read EMAIL
 ssh-keygen -f $HOME/.ssh/id_rsa -t rsa -b 4096 -C $EMAIL
 chmod 600 $HOME/.ssh/id_rsa
 
-# clean up
+# ======== #
+# clean up #
+# ======== #
 rm -f $HOME/.bash_history $HOME/.bash_logout $HOME/.bashrc

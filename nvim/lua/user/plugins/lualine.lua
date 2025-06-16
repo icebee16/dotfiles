ea536119a8e1e7ffde6d18eaa73b65fe3e85b7bf
nagamine koki <nagamine.k.aa@gmail.com>
2025-06-17 08:08:55 +0900

    feat(ui): add lualine.nvim for statusline

---
+++ nvim/lua/user/plugins/lualine.lua
@@ -0,0 +1,34 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("lualine").setup({
+      options = {
+        theme = "onedark",
+        icons_enabled = true,
+        section_separators = "",
+        component_separators = "|",
+        globalstatus = true,
+        disabled_filetypes = {
+          statusline = {},
+          winbar = {},
+        },
+        ignore_focus = {},
+        always_divide_middle = {},
+        refresh = {
+          statusline = 1000,
+          tabline = 1000,
+          winbar = 1000,
+        }
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff" },
+        lualine_c = { "filename" },
+        lualine_x = { "diagnostics", "encoding", "fileformat", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+    })
+  end,
+}

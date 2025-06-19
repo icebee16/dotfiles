46111fd01e27648f2f1a30e86defb4d65b80dc25
koki nagamine <nagamine.k.aa@gmail.com>
2025-06-20 07:47:36 +0900

    fix(lsp): connect lspconfig with cmp-nvim-lsp capabilities for completion

--- nvim/lua/user/plugins/lsp.lua
+++ nvim/lua/user/plugins/lsp.lua
@@ -33,6 +33,7 @@ return {
     },
     config = function()
       local lspconfig = require("lspconfig")
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
       -- Python
       lspconfig.pyright.setup({})

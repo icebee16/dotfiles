ab1911262967e4b455834ffa2dc4d17feccd52e7
koki nagamine <nagamine.k.aa@gmail.com>
2025-06-20 07:15:23 +0900

    feat(nvim): enable LSPs for python, lua, markdown, shell, and sql

---
+++ nvim/lua/user/plugins/lsp.lua
@@ -0,0 +1,60 @@
+return {
+  -- mason: LSP/DAP/Linter/Formatter installer
+  {
+    "williamboman/mason.nvim",
+    build = ":MasonUpdate",
+    config = true,
+  },
+
+  -- mason-lspconfig: bridge between mason and lspconfig
+  {
+    "williamboman/mason-lspconfig.nvim",
+    dependencies = { "mason.nvim" },
+    config = function()
+      require("mason-lspconfig").setup({
+        ensure_installed = {
+          "pyright",      -- Python
+          "lua_ls",       -- Lua (Neovim config)
+          "marksman",     -- Markdown
+          "bashls",       -- Shell (sh, bash)
+          "sqlls",        -- SQL
+        },
+        automatic_installation = true,
+      })
+    end,
+  },
+
+  -- lspconfig: Neovim LSP client configuration
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+      local lspconfig = require("lspconfig")
+
+      -- Python
+      lspconfig.pyright.setup({})
+
+      -- Lua (Neovim用設定)
+      lspconfig.lua_ls.setup({
+        settings = {
+          Lua = {
+            diagnostics = { globals = { "vim" } },
+            workspace = { checkThirdParty = false },
+          },
+        },
+      })
+
+      -- Markdown
+      lspconfig.marksman.setup({})
+
+      -- Shell
+      lspconfig.bashls.setup({})
+
+      -- SQL
+      lspconfig.sqlls.setup({})
+    end,
+  },
+}

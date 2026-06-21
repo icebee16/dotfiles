c6f0fff464c33d0ba660150a10d4fbc8211f93ad
koki nagamine <nagamine.k.aa@gmail.com>
2026-06-22 08:36:26 +0900

    fix(nvim): migrate treesitter config for neovim 0.12

--- nvim/lua/user/plugins/treesitter.lua
+++ nvim/lua/user/plugins/treesitter.lua
@@ -1,39 +1,39 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main",
+  lazy = false,
   build = ":TSUpdate",
+
   config = function()
-    require("nvim-treesitter.configs").setup({
-      ensure_installed = {
-        "c",
-        "lua",
-        "vim",
-        "vimdoc",
-        "query",
-        "elixir",
-        "heex",
-        "javascript",
-        "html",
-        "python",
-        "sql",
-        "markdown",
-        "markdown_inline",
-      },
-      sync_install = false,
+    local treesitter = require("nvim-treesitter")
 
-      highlight = {
-        enable = true,
-        disable = {
-          "markdown",
-          "markdown_inline",
-        },
-      },
+    treesitter.setup()
 
-      indent = {
-        enable = true,
-        disable = {
-          "markdown",
-        },
-      },
+    treesitter.install({
+      "c",
+      "lua",
+      "vim",
+      "vimdoc",
+      "query",
+      "elixir",
+      "heex",
+      "javascript",
+      "html",
+      "python",
+      "sql",
+      "markdown",
+      "markdown_inline",
+    })
+
+    local group = vim.api.nvim_create_augroup("user_treesitter_start", {
+      clear = true,
+    })
+
+    vim.api.nvim_create_autocmd("FileType", {
+      group = group,
+      callback = function(args)
+        pcall(vim.treesitter.start, args.buf)
+      end,
     })
   end,
 }
